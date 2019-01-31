e310fbd3a32173e350cfad21a5774e0e2af64deb
PlugMoose <PlugMoose@gmail.com>
2019-01-31 13:43:35 -0500

    branchs done

--- NoiseWork/Headers/Shapes.h
+++
@@ -1,9 +0,0 @@
-#ifndef  _SHAPES_H_
-#define  _SHAPES_H_
-struct Point;
-struct Spots_Hex;
-struct SDL_Hexagon;
-SDL_Hexagon Create_Hexagon(Point Center , Point Radius);
-SDL_Hexagon Move_Hexagon(SDL_Hexagon Old , Point New);
-int Draw_FillHexagon(SDL_Hexagon);
-#endif
